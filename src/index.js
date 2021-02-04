feb359309cd3e48158d30ba2e203c3128cf5e036
Davidcruz123 <daviducr.2593@gmail.com>
2021-02-04 16:16:20 +0000

    v2

--- src/index.js
+++ src/index.js
@@ -22,19 +22,19 @@ return (
       <Jumbotron />
 
       <div className="row">
-        <div className="col-4">
+        <div className="col-md-4">
           <Card
             title="Titulo1"
             link="https://cnnespanol.cnn.com/wp-content/uploads/2019/12/s_64a163f16ecbb099e52f2f8271f73cbbfcfc9034be4d646f7375e4db1ca6f3d7_1573501883482_ap_19001106049831-1.jpg?quality=100&strip=info&w=320&h=240&crop=1"
           />
         </div>
-        <div className="col-4">
+        <div className="col-md-4">
           <Card
             title="Titulo2"
             link="https://i.blogs.es/594843/chrome/450_1000.jpg"
           />
         </div>
-        <div className="col-4">
+        <div className="col-md-4">
           <Card
             title="Titulo3"
             link="https://i.blogs.es/3650f5/eclipsed-moon-trail-c-chuanjin-su/450_1000.jpg"
